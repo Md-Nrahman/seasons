2732352b605acedc7b8a53eb37ad970cf3111fdf
md-nrahman <md.naimur.rahman.naim@gmail.com>
2020-10-22 15:27:11 +0600

    Updates

---
+++ precache-manifest.34c10b5752c2a981faad3813490ead00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80fd7c6b6874b9c1ece6a6c7f454dbd0",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "3602e547edc17cec2476",
+    "url": "/seasons/static/css/main.82f5af3e.chunk.css"
+  },
+  {
+    "revision": "36079b390712337427b8",
+    "url": "/seasons/static/js/2.db57ba89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/seasons/static/js/2.db57ba89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3602e547edc17cec2476",
+    "url": "/seasons/static/js/main.635923b3.chunk.js"
+  },
+  {
+    "revision": "c9aa463c2c8102aff538",
+    "url": "/seasons/static/js/runtime-main.d04c89a8.js"
+  }
+]);
